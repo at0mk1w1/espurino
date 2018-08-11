da001708bae4a0899040982a2e04338c88e150aa
at0mk1w1 <kevin@duchrow.email>
2018-08-11 12:17:47 +0200

    Update test.js

--- test.js
+++ test.js
@@ -7,29 +7,27 @@ var C = {
    TEST : "HAHAHAHAAH" //Testconstante
 };
 
-function MOD123(pin1,pin2) {
+function test(pin1,pin2) {
   this.pin1 = pin1;
   this.pin2 = pin2;
 }
 
 /** 'public' constants here */
-MOD123.prototype.C = {
-  MY : 0x013,         // description
-  PUBLIC : 0x0541,    // description
-  CONSTANTS : 0x023   // description
+test.prototype.C = {
+  TEST : "HAHAHAHAAH" //Testconstante
 };
 
 /** Put most of my comments outside the functions... */
-MOD123.prototype.foo = function() {
+test.prototype.foo = function() {
   // you can use C.PRIVATE
   // or this.C.PUBLIC
 };
 
 /** Put most of my comments outside the functions... */
-MOD123.prototype.bar = function() {
+test.prototype.bar = function() {
 };
 
 /** This is 'exported' so it can be used with `require('MOD123.js').connect(pin1,pin2)` */
 exports.connect = function (pin1, pin2) {
-  return new MOD123(pin1, pin2);
+  return new test(pin1, pin2);
 };
